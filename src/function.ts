3025a9d49ce317fafc0b4c1af6b09b3ee08a76d7
653986462 <653986462>
2023-05-10 09:37:54 +0800

    init

---
+++ src/function.ts
@@ -0,0 +1,48 @@
+/**
+ * 防抖
+ * @param {Function} fn 添加防抖的函数
+ * @param {number} delay 防抖延迟，defult: 500
+ * @version 1.0
+*/
+export const debounce = function (fn: Function, delay: number = 500) {
+  let time: any = null
+  return function (...args: any) {
+    if (time !== null) {
+      clearTimeout(time)
+    }
+    time = setTimeout(() => {
+      // @ts-ignore
+      fn.apply(this, args)
+    }, delay);
+  }
+}
+
+/**
+ * 节流
+ * @param {Function} fn 添加节流的函数
+ * @param {number} wait 节流时间，defult: 500
+ * @version 1.0
+*/
+export const throttle = function (fn: Function, wait: number = 500) {
+  var timer: any = null;
+  var startTime = Date.now();
+
+  return function () {
+    var curTime = Date.now();
+    var remaining = wait - (curTime - startTime);
+    // @ts-ignore
+    var context = this;
+    var args = arguments;
+
+    clearTimeout(timer);
+
+    if (remaining <= 0) {
+      fn.apply(context, args);
+
+      startTime = Date.now();
+
+    } else {
+      timer = setTimeout(fn, remaining);
+    }
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as __function from './function'
+
+export default {
+  ...__function
+}
